2cade7615246fd7c6ecf7148d16c8a2fbf3d6082
Leonid Almanza <LeonidAlmanza@gmail.com>
2024-04-18 21:41:34 -0500

    commit3

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
         element: <Login />,
       },
       {
-        path:'/',
+        path:'/home',
         element: <ProtectedRoute />,
         children:[
           {
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { useState } from "react";
+import {useAuth} from "../auth/authProvider";
+import { Navigate } from "react-router-dom";
 
-import '../css.css'
+import '../css.css';
+
 
 function Login(){
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const auth = useAuth();
+
+    if(auth.isAuthenticated){
+        return <Navigate   to="/home"/>;
+    }
     return(
         <>
             <div class="container">
@@ -11,12 +21,16 @@ function Login(){
                     <form class="login">
                     <div class="login__field">
                         <i class="login__icon fas fa-user"></i>
-                        <input type="text" class="login__input" placeholder="User name / Email"/>
+                        <input type="text" class="login__input" placeholder="User name / Email" 
+                                value={name} 
+                                onChange={(e)=>setName(e.target.value)} />
                     </div>
                     <h2>desde login</h2>
                     <div class="login__field">
                         <i class="login__icon fas fa-lock"></i>
-                        <input type="password" class="login__input" placeholder="Password" />
+                        <input type="password" class="login__input" placeholder="Password" 
+                                value={password} 
+                                onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
                     <button class="button login__submit">
                         <span class="button__text">Ingresar</span>
