355d8033f76e8c37b427bf00692eab9f5885fd4d
Leonid Almanza <LeonidAlmanza@gmail.com>
2024-04-23 19:05:40 -0500

    commit3

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,20 @@
-export default function Home(){
-    return(
-            <h1>Principal</h1>
-        ); 
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import './home.css'
+
+const home = () => {
+  return (
+    <div>
+        <header class="banner">
+        <ul>
+            <li>Mensajes</li>
+            <li>Configuracion</li>
+            <li>Usuario</li>
+        </ul>
+        </header>
+        <aside class="menu"></aside>
+    </div>
+  )
 }
+
+export default home
