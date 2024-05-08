af4f3841b31a26f0b68301fc0c0a98c4d3c5c1a9
Leonid Almanza <LeonidAlmanza@gmail.com>
2024-05-07 22:38:52 -0500

    componentes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import '../styles/home.css'
 
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import '../styles/home.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
+import './banner.jsx';
 const home = () => {
   return (
-    <div>
-        <header class="banner">
-         <nav className='menu-container'>
-                <ul class="banner-ul">
-                    <li>Mensajes</li>
-                    <li>Configuracion</li>
-                    <li>Usuario</li>
-                    <li><img src="./logoCliente.png" alt="" /></li>
-                </ul>
-        </nav>
-        </header>
-        <aside class="menu"></aside>
-    </div>
+    <>
+      <header>
+          <ResponsiveAppBar></ResponsiveAppBar>
+      </header>
+    </>
   )
 }
 
 export default home
+
+
+
